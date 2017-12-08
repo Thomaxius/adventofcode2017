28f00071418974d93d5c201703bcdfac12c46a42
Tommi Santamaa <Tommi.Santamaa@gmail.com>
2017-12-08 22:24:48 +0200

    Fix linebreaks causing problems
    It worked cause I copy-pasted the input instead of saving it.

--- src/day4/day4_part1.py
+++ src/day4/day4_part1.py
@@ -1,4 +1,5 @@
 def solve(input):
+    input = input.rstrip('\n')
     return len([row for row in input.split('\n') if not (len(row.split(' ')) > len(set(row.split(' '))))])
 
 input = open("input.txt")
--- src/day4/day4_part2.py
+++ src/day4/day4_part2.py
@@ -1,19 +1,16 @@
 def solve(input):
-    input = input.split('\n')
+
+    input = input.rstrip('\n').split('\n')
     comparsion_list = []
     i = 0
     for row in input:
         wordlist = row.split()
         for word in wordlist:
-            if word:
-                comparsion_list.append(''.join(sorted(word)))
+            comparsion_list.append(''.join(sorted(word)))
         if len(wordlist) == len(set(comparsion_list)):
             i += 1
         comparsion_list = []
     return i
-
-
-
 
 input = open("input.txt")
 input = input.read()
