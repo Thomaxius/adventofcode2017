6faba1ca1e3118fff8ba451a85f0af3fc2f96576
Tommi Santamaa <Tommi.Santamaa@gmail.com>
2017-12-08 17:12:43 +0200

    Simplify day4 part1

--- src/day4/day4_part1.py
+++ src/day4/day4_part1.py
@@ -1,7 +1,5 @@
 def solve(input):
-    input = input.split('\n')
-    cleaned = [row for row in input if not (len(row.split(' ')) > len(set(row.split(' '))))]
-    return len(cleaned)
+    return len([row for row in input.split('\n') if not (len(row.split(' ')) > len(set(row.split(' '))))])
 
 input = open("input.txt")
 input = input.read()
